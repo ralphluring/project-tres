4f6523a0b33dedc9c040dafdef0a0204d0ec455c
dschneibolk <dschneibolk@gmail.com>
2019-04-29 14:40:11 -0700

    updated seed file

---
+++ models/Seeds.js
@@ -0,0 +1,31 @@
+db.places.insertMany(
+   {
+        "place_name": "California Surf Museum",
+        "place_img": "http://upload.wikimedia.org/wikipedia/commons/thumb/1/11/CA_Surf_Museum.jpg/250px-CA_Surf_Museum.jpg",
+        "place_location": [33.196500, -117.380300],
+        "place_visited" : false
+    },
+    {
+        "place_name": "Hut & Hangers",
+        "place_img": "http://blogs.chapman.edu/huell-howser-archives/1995/12/10/hut-hangers-californias-gold-602/",
+        "place_location": [33.383728, -117.374731],
+        "place_visited" : false
+    },
+    {
+        "place_name": "San Onofre Beach",
+        "place_img": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.californiabeaches.com%2Fwp-content%2Fuploads%2F2014%2F09%2FSan-Onofre-Bluffs-Nude-Beach-Bryce-Apr16-6-1000x657.jpg&imgrefurl=https%3A%2F%2Fwww.californiabeaches.com%2Fbeach%2Fsan-onofre-state-beach-nude-area%2F&docid=NauO1URMmn1kTM&tbnid=7owUtB3k_NoaBM%3A&vet=10ahUKEwjQlP3jnvbhAhXNJTQIHSI0B8AQMwhYKAAwAA..i&w=1000&h=657&safe=off&bih=700&biw=714&q=san%20onofre%20beach&ved=0ahUKEwjQlP3jnvbhAhXNJTQIHSI0B8AQMwhYKAAwAA&iact=mrc&uact=8",
+        "place_location": [33.384217, -117.583599],
+        "place_visited" : false
+    },
+    {
+        "place_name": "See's Candy",
+        "place_img": "http://upload.wikimedia.org/wikipedia/en/a/a8/See%27s_Candies_logo.png",
+        "place_location": [37.790129, -122.401208],
+        "place_visited" : false
+    },
+    {
+        "place_name": "Muybridge",
+        "place_img": "http://upload.wikimedia.org/wikipedia/en/thumb/b/b7/Stanford_University_seal_2003.svg/220px-Stanford_University_seal_2003.svg.png",
+        "place_location": [37.427461, -122.169708],
+        "place_visited" : false
+    })
