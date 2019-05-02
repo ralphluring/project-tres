eb0f328d743711cab168cbeaebbce0acd968378c
Ralph Luring <bennettchrislloyd@gmail.com>
2019-05-02 16:16:16 -0700

    made on click connect to db

--- client/src/components/pages/AllPlaces.js
+++ client/src/components/pages/AllPlaces.js
@@ -11,20 +11,25 @@ export default class PlaceList extends Component {
 
     state = { places: [] }
 
-    handleVisited = (e, id) => {
-        
+    handleVisited = async(e, id) => {
         console.log(id)
+        let value;
         this.setState({
             places: this.state.places.map( place => {
-                if( place._id !== id )
+                if( place._id !== id ) {
                     return place
-
+                }
+                value = !place.place_visited
                 return {
                     ...place,
                     place_visited: !place.place_visited
                 }
             })
         })
+        await axios.put(`http://localhost:5000/data/${id}`,{
+            place_visited: value
+        })
+
     }
 
     componentDidMount() {
